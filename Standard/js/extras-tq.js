505efabf1a6c6ca2db4067974655e3213848b282
Jordan Quigg <jordan.quigg@chde.qld.gov.au>
2023-10-03 14:46:51 +1000

    Add test courses

---
+++ Standard/js/extras-tq.js
@@ -0,0 +1,261 @@
+$(document).ready(function() {
+
+    $("#DigitalLiteracy_quiz").quiz({
+        "questions": [
+            {
+                "question": "Select the correct answers to what digital literacy means.",
+                "type": "tick",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Communication",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Collaboration",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Expression of ideas"
+                    },
+                    {
+                        "answer": "Staying in touch with friends or family",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Using work-based programs",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Using mobile apps",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Processing complaints"
+                    },
+                    {
+                        "answer": "Using digital literacy for study ",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Improving career opportunities",
+                        "correct": true
+                    }
+                ]
+            }
+        ],
+        "title": "Digital literacy"
+    })
+
+    $("#SmartDeviceAct").quiz({
+        "instructions": "In addition to the smart phone, what are some smart devices that have become an everyday part of our home and work life? <p>Please complete the question(s) below.</p>",
+        "questions": [
+            {
+                "question": "Fridge?",
+                "type": "multi",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Yes",
+                        "correct": true
+                    },
+                    {
+                        "answer": "No"
+                    }
+                ]
+            },
+            {
+                "question": "Washing machine?",
+                "type": "multi",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Yes",
+                        "correct": true
+                    },
+                    {
+                        "answer": "No"
+                    }
+                ]
+            },
+            {
+                "question": "Air conditioner?",
+                "type": "multi",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Yes",
+                        "correct": true
+                    },
+                    {
+                        "answer": "No"
+                    }
+                ]
+            },
+            {
+                "question": "Vaccum?",
+                "type": "multi",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Yes"
+                    },
+                    {
+                        "answer": "No",
+                        "correct": true
+                    }
+                ]
+            },
+            {
+                "question": "Television?",
+                "type": "multi",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Yes",
+                        "correct": true
+                    },
+                    {
+                        "answer": "No"
+                    }
+                ]
+            },
+            {
+                "question": "Security Cameras?",
+                "type": "multi",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Yes",
+                        "correct": true
+                    },
+                    {
+                        "answer": "No"
+                    }
+                ]
+            },
+            {
+                "question": "Garage door openers?",
+                "type": "multi",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Yes",
+                        "correct": true
+                    },
+                    {
+                        "answer": "No"
+                    }
+                ]
+            },
+            {
+                "question": "Light bulbs",
+                "type": "multi",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Yes",
+                        "correct": true
+                    },
+                    {
+                        "answer": "No"
+                    }
+                ]
+            },
+            {
+                "question": "Hair dryer?",
+                "type": "multi",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Yes"
+                    },
+                    {
+                        "answer": "No",
+                        "correct": true
+                    }
+                ]
+            }
+        ],
+        "title": "Smart devices"
+    })
+
+    $("#AppQuiz").quiz({
+        "questions": [
+            {
+                "question": "Choose a popular desktop application such as Word, Excel or PowerPoint. Open the application, and look for the toolbar on the top of the page. See what you are able to do.\nFrom the list below, select the correct answers:",
+                "type": "tick",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "open a new file",
+                        "correct": true
+                    },
+                    {
+                        "answer": "save it to your drive",
+                        "correct": true
+                    },
+                    {
+                        "answer": "enter text",
+                        "correct": true
+                    },
+                    {
+                        "answer": "format the text in bold, italics",
+                        "correct": true
+                    },
+                    {
+                        "answer": "retrieve and email"
+                    }
+                ]
+            },
+            {
+                "question": "Mobile phones are useful to make day-to-date tasks easier.  From the list below, select which are common mobile phone apps that you might normally use in handheld device rather than a computer.",
+                "type": "tick",
+                "feedback": {},
+                "answers": [
+                    {
+                        "answer": "Gmail -  email",
+                        "correct": true
+                    },
+                    {
+                        "answer": "PowerPoint – presentations"
+                    },
+                    {
+                        "answer": "Motor Mouth – tracks fuel prices in your location",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Handyman apps ",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Spirit level apps to indicate if a surface is horizontal ",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Distance measure apps to measure the distance between two points",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Flight Tracker – tracks where your plane is and if it’s on time",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Excel - Spreadsheets"
+                    },
+                    {
+                        "answer": "Weather Zone",
+                        "correct": true
+                    },
+                    {
+                        "answer": "Living earth – track the location, time, weather of anywhere in the world",
+                        "correct": true
+                    }
+                ]
+            }
+        ],
+        "title": "Apps Quiz"
+    })
+
+
+});
